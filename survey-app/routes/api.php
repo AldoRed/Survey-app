3f648477241a5c1dca2020b5d721a0c4b8ee944e
AldoRed <aldo@aldored.com>
2023-09-10 16:07:59 -0300

    Laravel Survey CRUD

--- survey-app/routes/api.php
+++ survey-app/routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:sanctum')->group(function (){
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('/surveys', SurveyController::class);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
