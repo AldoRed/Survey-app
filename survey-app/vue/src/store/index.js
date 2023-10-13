72ba93aad06c75e2a8efd3ce36947d188510ab92
AldoRed <aldo@aldored.com>
2023-10-13 00:24:18 -0300

    Add image to survey

--- survey-app/vue/src/store/index.js
+++ survey-app/vue/src/store/index.js
@@ -154,6 +154,7 @@ const store = createStore({
     },
     actions: {
         saveSurvey({ commit }, survey) {
+            delete survey.image_url;
             let response;
             if(survey.id) {
                 response = new Promise ((resolve, reject) => {
